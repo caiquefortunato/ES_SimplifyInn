7076167ae1a1d70c5cb4fa56a0e8fc13377855a2
lucasnm <lucasnm@lucasnm-X550CA>
2017-06-18 16:46:04 -0300

    Entidade de Pedido concluída

--- SimplifyInn/src/entidades/Pedido.java
+++ SimplifyInn/src/entidades/Pedido.java
@@ -7,8 +7,58 @@ package entidades;
 
 /**
  *
- * @author Philipe
+ * @author lucasnm
  */
 public class Pedido {
     
+    private int idQuarto;
+    private String servico;
+    private String OBSERVACOES;    
+    
+    private Pedido(){
+        
+    }
+
+    /**
+     * @return the idQuarto
+     */
+    public int getIdQuarto() {
+        return idQuarto;
+    }
+
+    /**
+     * @param idQuarto the idQuarto to set
+     */
+    public void setIdQuarto(int idQuarto) {
+        this.idQuarto = idQuarto;
+    }
+
+    
+    /**
+     * @return the servico
+     */
+    public String getServico() {
+        return servico;
+    }
+
+    /**
+     * @param servico the servico to set
+     */
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
+
+    /**
+     * @return the OBSERVACOES
+     */
+    public String getOBSERVACOES() {
+        return OBSERVACOES;
+    }
+
+    /**
+     * @param OBSERVACOES the OBSERVACOES to set
+     */
+    public void setOBSERVACOES(String OBSERVACOES) {
+        this.OBSERVACOES = OBSERVACOES;
+    }    
 }
